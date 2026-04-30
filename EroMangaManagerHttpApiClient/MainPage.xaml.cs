eaac79f88d4ac45172c224b772e2b7e07268130e
changbao zuo <2743109998@QQ.com>
2026-04-30 21:20:30 +0800

    把httpclient封装成一个类

--- EroMangaManagerHttpApiClient/MainPage.xaml.cs
+++ EroMangaManagerHttpApiClient/MainPage.xaml.cs
@@ -4,31 +4,26 @@ using System.Net.Http.Json;
 using EroMangaManager.Core.DTOs;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Uno.UI.Extensions;
+using Windows.System;
 
 namespace EroMangaManagerHttpApiClient;
 
 public sealed partial class MainPage : Page
 {
-    HttpClient client;
-    ObservableCollection<MangasGroupDTO> groups;
+    MangaAPIClient client = new MangaAPIClient();
+    ObservableCollection<MangasGroupDTO> groups = [];
+    ObservableCollection<MangaDTO> mangas = [];
     int currentpageindex;
     public MainPage()
     {
         this.InitializeComponent();
-        client = new HttpClient
-        {
-            BaseAddress = new Uri("http://192.168.1.2:5000/")
-        };
     }
     protected override async void OnNavigatedTo(NavigationEventArgs e)
     {
         base.OnNavigatedTo(e);
-        var folders = await client.GetFromJsonAsync<IEnumerable<MangasGroupDTO>>("/folders");
+        var folders = await client.GetGroupsBasicAsync();
         groups = new ObservableCollection<MangasGroupDTO>(folders);
         navigationview.MenuItemsSource = groups;
-        // var mangaList = await client.GetFromJsonAsync<ObservableCollection<MangaDTO>>("/mangas");
-        //System.Diagnostics.Debug.WriteLine(mangaList.Select(x=>x.Guid).ToList());
-        // gridview.ItemsSource = mangaList;
     }
 
 
@@ -44,7 +39,7 @@ public sealed partial class MainPage : Page
 
 
         // 直接下载到文件
-        var file = await client.GetStreamAsync($"/downloads/{manga.Guid}");
+        var file = await client.GetStreamAsync(manga.Guid);
         var progressring = container?.FindFirstDescendant<ProgressRing>();
         progressring.Visibility = Visibility.Visible;
 
@@ -55,7 +50,6 @@ public sealed partial class MainPage : Page
         progressring.Visibility = Visibility.Collapsed;
         AndroidOperation.Open(storagefile);
 
-        return;
         //TODO 下面可以显示下载进度，但安卓端有问题，暂时先直接下载后打开
         // 1.下载速度跑不满
         //2. progress.Report()调用会影响下载速度。，即便不使用progress.Report()，下载速度也只有2M多，怀疑是因为每次读写都要调用UI线程的关系。
@@ -65,112 +59,124 @@ public sealed partial class MainPage : Page
         //var root = container.ContentTemplateRootas StackPanel;
         //var progressbar = root?.FindName("progressbar") as ProgressBar;
 
-        var progressbar = container?.FindFirstDescendant<ProgressBar>();
+        //var progressbar = container?.FindFirstDescendant<ProgressBar>();
 
-        IProgress<float> progress = new Progress<float>(value =>
-        {
-            // 这里的代码会自动运行在 UI 线程上
-            progressbar.Value = value;
+        //IProgress<float> progress = new Progress<float>(value =>
+        //{
+        //    // 这里的代码会自动运行在 UI 线程上
+        //    progressbar.Value = value;
 
-        });
-        // 1. 先获取响应头，不要直接读内容流
-        var response = await client.GetAsync($"/downloads/{manga.Guid}", HttpCompletionOption.ResponseHeadersRead);
-        response.EnsureSuccessStatusCode();
+        //});
+        //// 1. 先获取响应头，不要直接读内容流
+        //var response = await client.GetAsync($"/downloads/{manga.Guid}", HttpCompletionOption.ResponseHeadersRead);
+        //response.EnsureSuccessStatusCode();
 
-        // 2. 获取总/近似大小(文件夹则按近似大小，单文件则按实际大小)，以之为进度条最大值。
-        long estimatedSize = 0;
-        if (response.Content.Headers.ContentLength.HasValue)
-        {
-            estimatedSize = response.Content.Headers.ContentLength.Value;
-        }
-        else if (response.Headers.TryGetValues("X-Estimated-Size", out var values))
-        {
-            estimatedSize = long.Parse(values.FirstOrDefault());
-        }
-        // 若大小为0，则不启用进度条
-        if (estimatedSize != 0)
-        {
-            progressbar.Visibility = Visibility.Visible;
-            progressbar.Maximum = (double)estimatedSize;
+        //// 2. 获取总/近似大小(文件夹则按近似大小，单文件则按实际大小)，以之为进度条最大值。
+        //long estimatedSize = 0;
+        //if (response.Content.Headers.ContentLength.HasValue)
+        //{
+        //    estimatedSize = response.Content.Headers.ContentLength.Value;
+        //}
+        //else if (response.Headers.TryGetValues("X-Estimated-Size", out var values))
+        //{
+        //    estimatedSize = long.Parse(values.FirstOrDefault());
+        //}
+        //// 若大小为0，则不启用进度条
+        //if (estimatedSize != 0)
+        //{
+        //    progressbar.Visibility = Visibility.Visible;
+        //    progressbar.Maximum = (double)estimatedSize;
 
-        }
+        //}
 
 
-        using var networkstream = await response.Content.ReadAsStreamAsync();
-        using (var fileStream = await storagefile.OpenStreamForWriteAsync())
-        {
-            var buffer = new byte[1048576]; // 64KB 缓冲区，提高下载速度。可选 1048576  65536
-            long bytesRead = 0;
-            int read;
+        //using var networkstream = await response.Content.ReadAsStreamAsync();
+        //using (var fileStream = await storagefile.OpenStreamForWriteAsync())
+        //{
+        //    var buffer = new byte[1048576]; // 64KB 缓冲区，提高下载速度。可选 1048576  65536
+        //    long bytesRead = 0;
+        //    int read;
 
-            while ((read = await networkstream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-            {
-                await fileStream.WriteAsync(buffer, 0, read);
+        //    while ((read = await networkstream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+        //    {
+        //        await fileStream.WriteAsync(buffer, 0, read);
 
-                bytesRead += read;
+        //        bytesRead += read;
 
-                // 3. 报告进度 (如果是 null 则跳过)
-                //if (estimatedSize > 0 && progress != null)
-                //{
-                //    // 计算百分比，这是默认progressbar是最大100的时候，不用了
-                //    //float percent =  (float)bytesRead / totalBytes.Value*100; 
+        //        // 3. 报告进度 (如果是 null 则跳过)
+        //        //if (estimatedSize > 0 && progress != null)
+        //        //{
+        //        //    // 计算百分比，这是默认progressbar是最大100的时候，不用了
+        //        //    //float percent =  (float)bytesRead / totalBytes.Value*100; 
 
-                //    progress.Report(bytesRead);
-                //}
-            }
-            //progress.Report(estimatedSize); // 确保最后报告完成
+        //        //    progress.Report(bytesRead);
+        //        //}
+        //    }
+        //    //progress.Report(estimatedSize); // 确保最后报告完成
 
-        }
+        //}
 
-        AndroidOperation.Open(storagefile);
+        //AndroidOperation.Open(storagefile);
 
     }
 
     private async void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
     {
         var menuItem = sender as MenuFlyoutItem;
+        var group = navigationview.SelectedItem as MangasGroupDTO;
 
         var manga = menuItem.DataContext as MangaDTO;
-        var group = navigationview.SelectedItem as MangasGroupDTO;
         if (group != null)
         {
-            group.MangaDTOs.Remove(manga);
-            numberbox.Maximum = (group.MangaDTOs.Count + 19) / 20;
+           _= mangas.Remove(manga);
+            var count=await client.GetMangasCountAsync(group.Guid);
+            numberbox.Maximum = (count + 19) / 20;
 
         }
-        var response = await client.DeleteAsync($"/mangas/{manga.Guid}");
+        var response = await client.DeleteAsync(manga.Guid);
         response.EnsureSuccessStatusCode();
 
     }
 
     private async void RefreshButton_Click(object sender, RoutedEventArgs e)
     {
+        mangas.Clear();
         var selectedGroup = navigationview.SelectedItem as MangasGroupDTO;
         if (selectedGroup != null)
         {
             var current = (int)numberbox.Value;
-            var mangadtos = selectedGroup.MangaDTOs.Skip((current - 1) * 20).Take(20).ToList();
-            gridview.ItemsSource = mangadtos;
+
+            var mangadtos= client.GetManyMangaDTOsAsync(selectedGroup.Guid, (current - 1) * 20, 20);
+
+            await foreach(var manga in mangadtos)
+            {
+                mangas.Add(manga);
+            }
+
         }
     }
 
 
 
-    private void Image_Loaded(object sender, RoutedEventArgs e)
-    {
-        var image = sender as Image;
-        var manga = image.DataContext as MangaDTO;
-        var uri = new System.Uri($"{client.BaseAddress}covers/{manga.Guid}");
-        image.Source = new BitmapImage(uri);
-    }
+    //private void Image_Loaded(object sender, RoutedEventArgs e)
+    //{
+    //    var image = sender as Image;
+    //    var manga = image.DataContext as MangaDTO;
+    //    var uri = new System.Uri($"{client.BaseAddress}covers/{manga.Guid}");
+    //    image.Source = new BitmapImage(uri);
+    //}
 
-    private void navigationview_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    private async void navigationview_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
     {
-        var groups = navigationview.SelectedItem as MangasGroupDTO;
-       
-        numberbox.Maximum = (groups.MangaDTOs.Count + 19) / 20;
+        var group = navigationview.SelectedItem as MangasGroupDTO;
+        var count =await client.GetMangasCountAsync(group.Guid);
+        numberbox.Maximum = (count+ 19) / 20;
         numberbox.Value = 1;
-        var mangadtos = groups.MangaDTOs.Take(20).ToList();
-        gridview.ItemsSource = mangadtos;
+        var mangadtos = client.GetManyMangaDTOsAsync(group.Guid,0, 20);
+        mangas.Clear();
+        await foreach(var manga in mangadtos)
+        {
+            mangas.Add(manga);
+        }
     }
 }
---
+++ EroMangaManagerHttpApiClient/MangaAPIClient.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+using EroMangaManager.Core.DTOs;
+using EroMangaManager.Core.Models;
+using ZstdSharp.Unsafe;
+
+namespace EroMangaManagerHttpApiClient;
+
+public class MangaAPIClient
+{
+    HttpClient client;
+    public MangaAPIClient()
+    {
+        client = new HttpClient
+        {
+            BaseAddress = new Uri("http://192.168.1.2:5000/")
+        };
+    }
+
+    public async Task<IEnumerable<MangasGroupDTO>> GetGroupsBasicAsync()
+    {
+        return await client.GetFromJsonAsync<IEnumerable<MangasGroupDTO>>("/folders/basicinfo");
+
+    }
+    public async Task<Stream> GetStreamAsync(string mangaGuid)
+    {
+        return await client.GetStreamAsync($"/downloads/{mangaGuid}");
+    }
+    public async Task<HttpResponseMessage> DeleteAsync(string mangaGuid)
+    {
+        return await client.DeleteAsync($"/mangas/{mangaGuid}");
+    }
+    public async IAsyncEnumerable<MangaDTO> GetManyMangaDTOsAsync(string groupGuid, int skip, int take)
+    {
+
+        await foreach (var mangaDTO in client.GetFromJsonAsAsyncEnumerable<MangaDTO>($"/folders/{groupGuid}/{skip}/{take}"))
+        {
+            mangaDTO.CoverUri = $"{client.BaseAddress}covers/{mangaDTO.Guid}";
+            yield return mangaDTO; 
+        }
+        
+    }
+    public async Task<int> GetMangasCountAsync(string groupGuid) {
+        var countstr = await client.GetStringAsync($"/folders/{groupGuid}/count");
+        var count = int.Parse(countstr);
+        return count;
+    }
+
+}
